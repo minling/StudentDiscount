90f1998564ae7ccc3d44a00a187df4f1f83f46f6
minling <mzhao725@gmail.com>
2015-12-06 16:39:22 -0500

    fixes locate me button to zoom into current map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,77 @@
-function getLocation() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 13
+var map;
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+};
+
+function showPosition(position) {
+  $('#location').html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
+};
+
+function initMap(){
+// debugger;
+// var latitude = locations.coords.latitude
+// var longitude = locations.coords.longitude
+
+var locations = gon.locations
+// [venue.name, venue.latitude, venue.longitude, venue.discount, venue.address, venue.tags.map { |tag| tag.name }]
+
+map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 12,
+  center: new google.maps.LatLng(40.7127, -74.0059),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+});
+
+var infowindow = new google.maps.InfoWindow();
+
+var marker, i;
+
+for (i = 0; i < locations.length; i++) {  
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+    map: map
   });
+
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+    return function() {
+          //setting the content 
+          infowindow.setContent('<h2>'+locations[i][0] +'</h2>' 
+            + '<div class="venue-info"><b>Discount: </b>' + locations[i][3] + '</div>'
+            + '<div class="venue-info"><b>Address: </b>' + locations[i][4] + '</div>'
+            + '<div class="venue-info"><b>Applies to: </b>' + locations[i][5] + '</div>');
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+  google.maps.event.addDomListener(window, "load", initMap);
+}
+}; 
+
+
+// function initMap() {
+
+//   var myLatLng = {lat: 40.7127, lng: -74.0059};
+//   var pinkberry = {lat: 40.747623, lng: -73.986145};
+
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//       center: myLatLng,
+//       zoom: 13
+//     });
+
+//   var marker = new google.maps.Marker({
+//     position: pinkberry,
+//     map: map,
+//     title: 'Pinkberry!'
+//   });
+
+//   marker.setMap(map);\
+// };
+function getLocation() {
+  // var map = new google.maps.Map(document.getElementById('map'), {
+  //   zoom: 13
+  // });
   // var infoWindow = new google.maps.InfoWindow({map: map});
   var infoWindow = new google.maps.Marker({
     map:map,
@@ -22,6 +92,7 @@ function getLocation() {
       infoWindow.setPosition(pos);
       // infoWindow.setContent('You are here');
       map.setCenter(pos);
+      map.setZoom(15);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
@@ -30,72 +101,3 @@ function getLocation() {
     handleLocationError(false, infoWindow, map.getCenter());
   }
 }
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-};
-
-function showPosition(position) {
-  $('#location').html("Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude);
-};
-
-function initMap(){
-// debugger;
-// var latitude = locations.coords.latitude
-// var longitude = locations.coords.longitude
-
-var locations = gon.locations
-// [venue.name, venue.latitude, venue.longitude, venue.discount, venue.address, venue.tags.map { |tag| tag.name }]
-
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 12,
-      center: new google.maps.LatLng(40.7127, -74.0059),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var marker, i;
-
-    for (i = 0; i < locations.length; i++) {  
-      marker = new google.maps.Marker({
-        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-        map: map
-      });
-
-      google.maps.event.addListener(marker, 'click', (function(marker, i) {
-        return function() {
-          //setting the content 
-          infowindow.setContent('<h2>'+locations[i][0] +'</h2>' 
-                                + '<div class="venue-info"><b>Discount: </b>' + locations[i][3] + '</div>'
-                                + '<div class="venue-info"><b>Address: </b>' + locations[i][4] + '</div>'
-                                + '<div class="venue-info"><b>Applies to: </b>' + locations[i][5] + '</div>');
-          infowindow.open(map, marker);
-        }
-      })(marker, i));
-google.maps.event.addDomListener(window, "load", initMap);
-    }
-}; 
-
-
-// function initMap() {
-
-//   var myLatLng = {lat: 40.7127, lng: -74.0059};
-//   var pinkberry = {lat: 40.747623, lng: -73.986145};
-
-//   var map = new google.maps.Map(document.getElementById('map'), {
-//       center: myLatLng,
-//       zoom: 13
-//     });
-
-//   var marker = new google.maps.Marker({
-//     position: pinkberry,
-//     map: map,
-//     title: 'Pinkberry!'
-//   });
-
-//   marker.setMap(map);
-// };
